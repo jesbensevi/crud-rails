005f5f1395d14effa0fb28cf16795b4c5cf70e31
Jesben <jesben.sevillano.h@uni.pe>
2021-04-25 22:18:21 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'postres/index'
-  get 'postres/read'
-  get 'postres/create'
-  get 'postres/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'postres/index' => 'postres#index' # Ruta de la vista principal de los registros 
+  get 'postres/leer/:url', to: 'postres#leer' # Ruta de la vista leer o ver los detalles de un registro 
+  get 'postres/crear' => 'postres#crear' # Ruta de la vista para crear un registro 
+  post 'postres/insertar' => 'postres#insertar' # Ruta que procesa la creación de un registro en la base de datos 
+  get 'postres/actualizar/:id', to: 'postres#actualizar' # Ruta de la vista para actualizar un registro 
+  post 'postres/editar/:id', to: 'postres#editar' # Ruta que procesa la actualización de un registro en la base de datos 
+  post 'postres/eliminar/:id', to: 'postres#eliminar' # Ruta para eliminar un registro de la base de datos 
+
 end
